0aab4ed22483f139a90f5d86eb4c304d0957b042
wipark2020 <78107368+wipark2020@users.noreply.github.com>
2024-05-10 23:35:58 -0400

    paritful

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -23,10 +23,6 @@ function Project1() {
             <p className='content'>
                 Unfortunately, many of Partiful's users faced usability challenges with its homepage, limiting user engagement. The original design made it difficult for users to view multiple events and lacked features to engage users without upcoming events. 
             </p>
-            <div className='problem-image'>
-                <img src={process.env.PUBLIC_URL + "/images/problem.png"} alt="Homepage problems" />
-                {/* Add CSS to position pointers over this image */}
-            </div>
 
             <h2 className='subtitle-content'>Objective</h2>
             <p className='content'>
@@ -73,23 +69,29 @@ function Project1() {
                 <b>Note:</b> To start, we were given the ability to meet with the clients once a week to get feedback on the project to lead us in the right direction. 
             </p>
             <p className= 'disclaimer'>
-                <b>Note:</b> Since I was working with a team, the work I did is colored in blue.
+                <b>Note:</b> Since I was working with a team, the work I did is colored in <span style={{ color: 'blue' }}>blue</span>.
             </p>
             <p className='content'>
                 Once we established the main painpoints of Partiful's homepage, we decided to get inspiration at other apps that were social and one-dimensional where one-dimensional means that the app is targeting a specific niche like restaurants or running which is similar to events. 
             </p>
-            {/* Add a carousel of images of the apps Beli, Netflix, and Strava with a link and description of what we took  */}
 
             <p className='content'>
                 Drawing inspiration from popular apps like Beli for its categorical navigation, Netflix for its engaging content display, and Strava for its community features, we sketched various redesigns to incorporate these elements effectively.
             </p>
-            {/* Add figma lowfi app screens  */}
 
             <p className='content'>
-                We decided to make a carousel wheel to view the events as we thought it was a cool interaction to see multiple events where you can see the next and the event before it. 
+                We decided to make a <span style={{ color: 'blue' }}>carousel wheel</span> to view the events as we thought it was a cool interaction to see multiple events where you can see the next and the event before it. 
                 We used the Beli app as inspiration for the different categories that you can view the events in. This was to make the app more engaging and fun to use as there is content to view regarding what parties are trending or what themes to do.
                 It also contributed to the Partifulness that Partiful wanted to have in their app.
             </p>
+
+            <div style={{ display: 'flex', justifyContent: 'center', padding: '10px'}}>
+                <iframe
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)', width: '570px', height: '450px' }}
+                  src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FZ8QRNMO1ghxuZLzQT6Ah7s%2FPartiful%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D41-1374%26viewport%3D345%252C928%252C0.05%26t%3D3NB3n3E9nnTqRzCL-1%26scaling%3Dcontain%26starting-point-node-id%3D41%253A1374%26show-proto-sidebar%3D1%26mode%3Ddesign"
+                  allowFullScreen
+                />
+            </div>
 
             <p className='content'>
             We iterated based on weekly feedback sessions with Partiful, focusing on simplifying the interface and enhancing accessibility. The following issues were brought up:
@@ -100,13 +102,20 @@ function Project1() {
             </ul>
 
             <p className='content'>
-            Thus, in investigating the problem, we decided to go with a different design that would meet the client's needs, which was through the "Apple Wallet" style of viewing the credit cards.
+            Thus, in investigating the problem, we decided to go with a different design that would meet the client's needs, which was <span style={{ color: 'blue' }}>through the "Apple Wallet" style</span> of viewing the credit cards.
             </p>
             {/* image of apple wallet and description of why it is good on the side of it  */}
             <p className='content'>
             The final design adopted an "Apple Wallet" style for event cards, providing a clean, easily navigable interface. To keep the app different in interactions, we kept the Netflix style of scrolling on both the "Trends" and "Friends" pages.
+            <span style={{ color: 'blue' }}>The "Events" tab also included other features such as a "Create Event" button and easier ways to observe Past events as well. There are also different homepages to consider the different scenarios of a person having an event, no event, or having an invite.</span>
             </p>
-            {/* Add figma hifi app screens  */}
+            <div style={{ display: 'flex', justifyContent: 'center', padding: '10px'}}>
+                <iframe
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)', width: '570px', height: '450px' }}
+                  src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FZ8QRNMO1ghxuZLzQT6Ah7s%2FPartiful%3Fpage-id%3D23%253A50%26type%3Ddesign%26node-id%3D189-3966%26viewport%3D-2239%252C249%252C0.6%26t%3DHDiGS1K7rwi0hczr-1%26scaling%3Dscale-down%26starting-point-node-id%3D189%253A3966%26show-proto-sidebar%3D1%26mode%3Ddesign"
+                  allowFullScreen
+                />
+              </div>
 
             <h2 className='subtitle-content'>Impact</h2>
             <p className='content'>
