01c6a58269aee81d024ffbb3b337efd2cc9d73d5
wipark2020 <78107368+wipark2020@users.noreply.github.com>
2024-05-11 01:16:18 -0400

    finish

--- src/components/Project3.js
+++ src/components/Project3.js
@@ -4,38 +4,98 @@ import './Project.css'; // Importing the CSS
 function Project3() {
     return (
         <div className='body'>
-            <h1 className='title'>FIFAaddict Redesign</h1> {/* Title in h1 for semantic importance */}
+            <h1 className='title'>FIFAaddict</h1> {/* Title in h1 for semantic importance */}
             <h2 className='subtitle'>What do you use to get information on the video game FIFA?</h2> {/* Subtitle */}
             <img src={process.env.PUBLIC_URL + "/images/homepage.png"} alt="FIFAaddict homepage redesign" className="project-image" />
 
-            <h2 className='subtitle-content'>Problem & Background</h2>
+            <h2 className='subtitle-content'>Background</h2>
+            <p className='content'>FIFAaddict is a useful tool for the video game FIFA Online 4 which is a free-to-play multiplayer football (soccer) game. FIFAaddict offers a player database and tools for FIFA Online 4 players to search and compare player stats, calculate transfer tax, and build teams with updated data of the game.</p>
+            <p className='content'>Link to Website: <a href="https://en.fifaaddict.com/">FifaAddict Website</a></p>
             <p className='content'>
-                FIFAaddict is a popular tool among FIFA Online 4 gamers but suffered from usability and accessibility issues. The cluttered layout, poor visual hierarchy, and inadequate color contrasts hindered user experience. Motivated by personal experience and feedback from fellow players, I undertook the redesign to enhance navigation and overall functionality.
+                FIFAaddict is a popular tool among FIFA Online 4 gamers. It is something I use daily and around 3 million other users as well. However, it suffers from usability and accessibility issues. The cluttered layout, poor visual hierarchy, and inadequate color contrasts hindered user experience. Motivated by personal experience and feedback from fellow players, I undertook the redesign to enhance navigation and overall functionality.
             </p>
 
             <h2 className='subtitle-content'>User Research</h2>
             <p className='content'>
-                User research involved analyzing competitor sites, collecting feedback from current users, and understanding the core functionalities that users valued most. This helped identify the essential features needed on the homepage and guided the redesign of user interactions.
+                I conducted personal research on the website and gathered online feedback from fellow FIFA Online 4 players. The main <b>usability</b> issues identified were:
             </p>
-
+            <ol className='content-list'>
+                <li>The website does not have a clear visual hierarchy as the site has a lot going on visually where it could benefit from emphasizing information, not allowing the user to strategically guide the user's attention.</li>
+                <li>It does not have a common scan pattern such as ‘F’ or ‘Z’ pattern where a more structured layout could help in improving the navigation since the interface is so cluttered</li>
+                <li>The color and layout also do not help in distinguishing the different sections so utilizing more color could help in organizing the layout</li>
+                <li>The site could also utilize some gestalt principles where grouping similar items could be more grouped together such as buttons</li>
+                <li>There is too much on the home page that makes it difficult to follow where one feature starts or ends</li>
+                <li>The links at the top of the page are unknown of what they are so it is hard to navigate the website</li>
+            </ol>
+            <img src={process.env.PUBLIC_URL + "/images/WebAIM.png"} alt="WebAIM" class="picture"className="project-image"/>
+            <p className='content'>
+                I was able to test accessibility using WebAIM analysis. The main <b>accessibility</b> issues identified were:
+            </p>
+            <ol className='content-list'>
+            <li>Color contrast is a big problem where the problem is “very low contrast” which makes it hard to see the features</li>
+            <li>Although aria labels are used, alternative text is missing which make it hard for users with screen readers to use the webpage.</li>
+            <li>The text size made it difficult to read as well</li>
+            </ol>
             <h2 className='subtitle-content'>Objective</h2>
             <p className='content'>
-                The main goal was to simplify the homepage to focus on the search functionality, improve accessibility standards, and ensure a clear visual hierarchy that enhances user flow and engagement.
+                The main goal was to simplify the homepage to focus on the search functionality, improve accessibility standards, and ensure a clear visual hierarchy that enhances user flow and engagement which all come from the problems of usability and accesibility that I found.
+                Most of the problems come from the cluttering of the homepage where there were too many features present in the home screen to notify new users where to navigate or how to easily use the website. 
+        My goals:
             </p>
-
+            <ol className='content-list'>
+          <li>Highlight the main feature of the website which is the search bar and the results which are the players</li>
+          <li>Separate the other features and link those to different home pages via links</li>
+          <li>Update the color contrasts and add accessibility features</li>
+          <li>Increase text size</li>
+      </ol>
             <h2 className='subtitle-content'>Process</h2>
-            <p className='content'>
-                Starting with quick sketches, I moved to creating lo-fi wireframes that reorganized the layout to emphasize the search bar and de-clutter the interface. After iterative feedback, hi-fi prototypes were developed to refine visual elements and interactivity.
-            </p>
+            <p className='content'>Knowing what the problems were, I started by sketching out the homepage and the different features that I wanted to include. I changed the design to be more modern where the players will be in cards and there will be an apparent search bar for easy access. The search bar is less cluttered where all the filters are not showing, but if you do want more filters, you can choose to open the filter button to do so.  </p>
+           
+            <div style={{ display: 'flex', justifyContent: 'center', padding: '10px'}}>
+                <iframe
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)', width: '570px', height: '450px' }}
+                  src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fy0wRvvlnPlLkbQDMYDcVrX%2FLo-fi%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DUyfi7LRMzbwTwGmj-1"
+                  allowFullScreen
+                />
+            </div>
+
+            <p className='content'>Getting feedback from peers regarding my lofi design, I decided it followed good design practices and standards with a modern design, so I put it in a Hi-Fi Prototype.</p>
+           
+            <div style={{ display: 'flex', justifyContent: 'center', padding: '10px'}}>
+                <iframe
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)', width: '570px', height: '450px' }}
+                  src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FuG8iB5zlRBObVyyKa0mrcP%2FResponsive-Redesign%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DGP3PZqNcACkPu1T9-1"
+                  allowFullScreen
+                />
+            </div>
+
+            <p className='content'>I then tested the Hi-Fi Prototype with users and got feedback on the design. The feedback was positive where users liked the modern design and the ease of access to the search bar. The search bar was the main feature that users wanted to use and the design made it easy to access. The filters were also easy to access and the design was clean and modern. So, I was able to code it fully.</p>
+            <p className='content'>Link to Website: <a href="https://wipark2020.github.io/responsive-redesign/">Responsive Redesign</a></p>
+            <p className='content'>Reasonings behind my redesigns: </p>
+            <ol className='content-list'>
+            <li>I really wanted the focus to be on the search bar. I removed most of the buttons that filter the website and just made it all condensed into an add filters button where it will be established that all the filters on the homepage originally will be accessed through that button. </li>
+            <li>Organizing the player cards also was similar. I kept the comparison with the three traits where the option button can change the traits.</li>
+            <li>The homepage had two other features other than the search function that I decided to just add to the sandwich menu button area which is the "Latest Comments" feature and the "Tax Calculator". 
+                Both are important features to players but they are not that important to include on the front page. This is because as you do decrease the screen size of the original website, both the features just disappear from the website. 
+                So, I decided to do the same (although I did not implement the sandwich button nor the add filters+ button). </li>
+            </ol>
+
 
             <h2 className='subtitle-content'>Impact</h2>
             <p className='content'>
-                The redesign led to a more intuitive and accessible interface, demonstrated by positive user feedback and improved engagement metrics. The clearer layout and enhanced navigation facilitated quicker and more efficient user interactions.
+                The redesign led to a more intuitive and accessible interface. It answers the problems that I had originally which include: establishing a clear hierarchy where it is clear what the title is and what the subsections are. 
+                It does follow a common scan pattern where it follows the F pattern. Along iwth this, doing an accessibility analysis through WebAIM, I was able to fix the color contrast and add alternative text to the images.
             </p>
-
+            <div className='project-content-image-2'>
+                <img src={process.env.PUBLIC_URL + "/images/final_webAIM.png"} alt="webAIM" />
+                <div>
+                    <p className='content'>Along with this, doing an accessibility analysis through WebAIM, I was able to fix the color contrast and add alternative text to the images. There are <b>no errors</b>. </p>
+                </div>
+            </div>
             <h2 className='subtitle-content'>Next Steps</h2>
             <p className='content'>
-                Future updates will focus on enhancing mobile responsiveness and adding more personalized features based on user data and trends. Continuous testing and feedback will guide further improvements to ensure the platform meets evolving user needs.
+                This case study helped me build a foundation in learning how to improve accesibility and improve content hierarchy. I am proud of my final product and hope to use these skills I learned in future projects.
+                In terms of what is next for this case study, future updates will focus on enhancing mobile responsiveness which is an important factor of accessibility as well as making it look more unique rather than a simple website that everyone has these days. 
             </p>
         </div>
     );
